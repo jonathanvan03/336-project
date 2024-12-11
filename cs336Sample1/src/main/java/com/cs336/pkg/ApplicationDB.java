e35057e1f75cec96292d83e19461e6c748279f5c
Jonathan Van <jonathan.van003@gmail.com>
2024-12-10 22:39:13 -0500

    changed ApplicationDB.java

--- cs336Sample1/src/main/java/com/cs336/pkg/ApplicationDB.java
+++ cs336Sample1/src/main/java/com/cs336/pkg/ApplicationDB.java
@@ -31,7 +31,7 @@ public class ApplicationDB {
 		}
 		try {
 			//Create a connection to your DB
-			connection = DriverManager.getConnection(connectionUrl,"root", "Pop-ems5384mysql");
+			connection = DriverManager.getConnection(connectionUrl,"root", "Cs336project");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
